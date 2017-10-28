ea9e28514085d4ea93eccc27b6ea89bbe06c404b
Ajith Thampi <ajith.thampi@evamiatic.com.au>
2017-10-28 14:32:49 +1100

    move away playbooks to play

---
+++ files/sql/01_create_identification_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `identification` 
+(
+  `userID` tinyint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `Name` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `Description` mediumtext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`userID`),
+) DEFAULT ;
