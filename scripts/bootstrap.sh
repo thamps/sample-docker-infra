5ad22027a449f8056050acfcdd651b622c108c55
Ajith Thampi <ajith.thampi@evamiatic.com.au>
2017-10-30 08:24:36 +1100

    re-order the bootstrap package install

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,9 +4,9 @@
 cat /home/vagrant/.ssh/runner.pub >> /home/vagrant/.ssh/authorized_keys 
 
 #Update the ansible binaries to latest. Current is 2.0.x
+apt-get -y install software-properties-common
 apt-add-repository ppa:ansible/ansible
 apt-get update
-apt-get -y install software-properties-common
 apt-get -y install ansible python-pip
 pip install docker-py
 
